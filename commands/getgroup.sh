61aab0ddbe158d8378ad5944710a86b6da30ae95
Joao Almeida <jcfd.almeida@campus.fct.unl.pt>
2021-04-04 17:04:29 +0100

    header name

---
+++ commands/getgroup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Set env variables such as GitHub TOKEN
+source .app.env
+source .token.env
+source .number.env
+
+repoName=$(curl https://gatherchain-app.azurewebsites.net/users/${STU_NUMBER} | jq -r '.GroupName')
+
+echo ${repoName}
--- commands/history.sh
+++ commands/history.sh
@@ -4,4 +4,4 @@
 #if [ "$1" == "-diff" ]; then
 #git log -p
 #else
-git log --pretty=format:'%h was %an, %ar, message: %s'
+git log --pretty=format:'%h by %an, %ar, message: %s'
