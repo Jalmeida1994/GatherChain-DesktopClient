8422484ecfe8efda97a5218c8aacbc17747eec14
31 <jcfd.almeida@campus.fct.unl.pt>
2021-04-17 12:46:11 +0100

    31: async

--- commands/push.sh
+++ commands/push.sh
@@ -5,7 +5,7 @@ source .app.env
 source .token.env
 source .number.env
 
-#cd $1
+#cd $2
 
 username=$(curl --fail -X GET -H "Accept: application/vnd.github.v3+json" -u ${username}:${ACCESS_TOKEN} https://api.github.com/user | jq -r '.login')
 
@@ -16,7 +16,7 @@ git config user.name "${STU_NUMBER}"
 git add .
 
 #First commit to master branch
-git commit -m "Commit by student nr: ${STU_NUMBER}"
+git commit -m "${STU_NUMBER}: ${1}"
 git branch -M master
 
 #Saves Commit id and prints to console
@@ -33,13 +33,13 @@ members=$(echo $GRP_NAME | tr "-" "\n")
 #Requests to create the network with the first group 
 echo "Pushing the commit hash to the blockchain network in group ${GRP_NAME}..."
 
-if curl --fail -X POST -H "Content-Type: application/json" -d "{\"Author\":\"${STU_NUMBER}\",\"Group\":\"${GRP_NAME}\",\"Commit\":\"${GIT_HASH}\"}" https://gatherchain-app.azurewebsites.net/push; then
-printf "Hash ${GIT_HASH} commited to the Network! Pushing to remote repo."
+#if curl --fail -X POST -H "Content-Type: application/json" -d "{\"Author\":\"${STU_NUMBER}\",\"Group\":\"${GRP_NAME}\",\"Commit\":\"${GIT_HASH}\"}" https://gatherchain-app.azurewebsites.net/push; then
+#printf "Hash ${GIT_HASH} commited to the Network! Pushing to remote repo."
 #Add the remote repo to the .git and pushes
 git pull
 git push -u origin master
-else
-printf "Error pushing the latest commit to the network: ${GIT_HASH}!"
-git reset --soft HEAD~1
-fi;
+#else
+#printf "Error pushing the latest commit to the network: ${GIT_HASH}!"
+#git reset --soft HEAD~1
+#fi;
 
