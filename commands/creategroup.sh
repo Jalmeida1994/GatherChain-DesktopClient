e787e10e8f7b62ee7d734adb88a02fbf78bd5e67
Jalmeida1994 <jcfd.almeida@campus.fct.unl.pt>
2021-05-02 15:09:29 +0100

    testing

--- commands/creategroup.sh
+++ commands/creategroup.sh
@@ -31,15 +31,15 @@ source ${1}/../.weburl.env
 
 
 # Getting the authenticated user's username
-jsonRes=$(curl --fail -X GET -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${ACCESS_TOKEN}" "https://api.github.com/user")# | jq -r '.login')
+jsonRes=$(curl --fail -X GET -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${ACCESS_TOKEN}" "https://api.github.com/user")  # | jq -r '.login')
 username=$(parse_json "${jsonRes}" login)
 
 # Parsing the script's input
-i=1;
+i=2;
 echo $@
-for student in "$@" 
+for student in "${@:$i}" 
 do
-    if [ $i -eq 1 ]
+    if [ $i -eq 2 ]
     then
         author=${STU_NUMBER};
         groupname="${STU_NUMBER}"
@@ -55,17 +55,12 @@ do
         i=$((i + 1));
         numbers+=${student}
         echo $numbers
-        jsonResponse=($(curl ${WEB_URL}/users/${student})# | jq -r '.GitHub'))
+        jsonResponse=$(curl ${WEB_URL}/users/${student})  # | jq -r '.GitHub'))
         usernames+=$(parse_json "${jsonRes}" GitHub)
         echo $usernames
         echo $i
     fi
 done
-
-if [ ${author} != ${STU_NUMBER} ]; then
-    echo ERROR: That is not the registered student number.
-    exit 1 # terminate and indicate error
-fi
 
 echo "Creating the group's ${groupname} repository in the GitHub's user: ${username}."
 
@@ -76,32 +71,34 @@ curl -X POST -H "Accept: application/vnd.github.v3+json" -u ${username}:${ACCESS
 curl -X PUT -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${username}/${groupname}/collaborators/${ADMIN_GITHUB}} -d '{"permission":"pull"}' 
 
 # Invites collaborators
-echo "Total colaboradores: ${#usernames[@]}"
-for w in ${#usernames[@]}
+echo "Total colaboradores: ${#usernames[@:3]}"
+for w in ${#usernames[@]:3}
 do
-    echo "https://api.github.com/repos/${username}/${groupname}/collaborators/${usernames[w-1]}}"
+    echo "https://api.github.com/repos/${username}/${groupname}/collaborators/${usernames[w-1]}"
     echo "Username: ${usernames[w-1]}"
-    curl -X PUT -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${username}/${groupname}/collaborators/${usernames[w-1]}} -d '{"permission":"admin"}' 
+    curl -X PUT -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${username}/${groupname}/collaborators/${usernames[w-1]} -d '{"permission":"admin"}' 
     # Gets the authenticated GitHub user
-    if curl --fail -X POST -H "Content-Type: application/json" -d "{\"Author\":\"${numbers[w-1]}\",\"GitHub\":\"${usernames[w-1]}\",\"Group\":\"${username}\",\"GroupName\":\"${groupname}\"}" ${WEB_URL}/registernumber; then
+    if [ curl --fail -X POST -H "Content-Type: application/json" -d "{\"Author\":\"${numbers[w-1]}\",\"GitHub\":\"${usernames[w-1]}\",\"Group\":\"${username}\",\"GroupName\":\"${groupname}\"}" ${WEB_URL}/registernumber ] 
+    then
         printf "Updated group for ${numbers[w-1]}!"
     else
         printf "Error updating group for the student ${numbers[w-1]}"
-    fi;
+    fi
 done
 
 cd $2
+
 #Inits local repo and adds ReadME.md
 git init
 git config author.name "${STU_NUMBER}" 
 git config user.name "${ADMIN_GITHUB}"
-git add .
+git add -A
 
-echo "{\"Author\":\"${STU_NUMBER}\",\"Group\":\"${groupname}\",\"FirstCommit\":\"${FIRST_GIT_HASH}\"}" >> .gatherchain.json
-echo ".gatherchain.json" >> .gitignore
+#echo "{\"Author\":\"${STU_NUMBER}\",\"Group\":\"${groupname}\",\"FirstCommit\":\"${FIRST_GIT_HASH}\"}" >> .gatherchain.json
+#echo ".gatherchain.json" >> .gitignore
 
 #First commit to master branch
-git commit -m "first commit"
+git commit -m "Creating group"
 git branch -M master
 
 #Add the remote repo to the .git and pushes
@@ -120,12 +117,13 @@ cd ${cwd}
 #Requests to create the network with the first group 
 echo "Creating a new channel in the Blockchain Network. This may take a minute... or two..."
 
-if curl --fail -X POST -H "Content-Type: application/json" -d "{\"Author\":\"${STU_NUMBER}\",\"Group\":\"${groupname}\",\"Commit\":\"${FIRST_GIT_HASH}\"}" ${WEB_URL}/creategroup; then
+if [ curl --fail -X POST -H "Content-Type: application/json" -d "{\"Author\":\"${STU_NUMBER}\",\"Group\":\"${groupname}\",\"Commit\":\"${FIRST_GIT_HASH}\"}" ${WEB_URL}/creategroup ] 
+then
 printf "Created group: ${groupname}!"
 else
 printf "Error creating the group: ${groupname}!"
 # TODO: remove for tests
-source ${1}/../commands/clear.sh ${1} ${2}
+#source ${1}/../commands/clear.sh ${1} ${2}
 exit "Error creating the group: ${groupname}!"
-fi;
+fi
 
